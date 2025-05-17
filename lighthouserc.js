ab88d3de69356fc9c78c0f5a205215cf0a875ef7
gdvolya <gdvolya@gmail.com>
2025-05-17 13:41:31 +0300

    Fix: корректный путь к HTML-отчётам Lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,22 +1,22 @@
 module.exports = {
   ci: {
     collect: {
-      url: ["https://promelektroservice.vercel.app"],
+      url: ['https://promelektroservice.vercel.app'],
       numberOfRuns: 1,
-      output: "html",
+      output: ['html'],
       settings: {
-        preset: "desktop",
+        preset: 'desktop',
       },
     },
     upload: {
-      target: "temporary-public-storage",
+      target: 'temporary-public-storage',
     },
     assert: {
       assertions: {
-        "categories:performance": ["warn", { minScore: 0.8 }],
-        "categories:accessibility": ["warn", { minScore: 0.9 }],
-        "categories:best-practices": ["warn", { minScore: 0.9 }],
-        "categories:seo": ["warn", { minScore: 0.9 }],
+        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
       },
     },
   },
