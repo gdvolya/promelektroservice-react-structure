5830dfc2b72ed9390e1c8b57130422ac920c2a6d
gdvolya <gdvolya@gmail.com>
2025-05-19 12:03:10 +0300

    Fix: сохраняем HTML-отчёты Lighthouse и загружаем как артефакт

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,21 +2,18 @@ module.exports = {
   ci: {
     collect: {
       url: ['https://promelektroservice.vercel.app'],
-      numberOfRuns: 1,
-      output: ['html'],
+      numberOfRuns: 3,
       settings: {
         preset: 'desktop',
       },
     },
     upload: {
       target: 'temporary-public-storage',
+      output: ['html'],
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
-        'categories:accessibility': ['warn', { minScore: 0.9 }],
-        'categories:best-practices': ['warn', { minScore: 0.9 }],
-        'categories:seo': ['warn', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: 0.9 }],
       },
     },
   },
