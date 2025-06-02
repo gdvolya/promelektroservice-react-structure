759f99a79c6c35e3e83e88fe5b6fbd438ac9bd06
gdvolya <gdvolya@gmail.com>
2025-06-02 09:40:19 +0300

    Refactor: ленивые маршруты и Firebase только для AdminPanel, обновления Lighthouse отчетов

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import SeoHelmet from "../components/SeoHelmet";
+
 import project1 from "../img/project1.webp";
-// импорт остальных проектов...
+import project2 from "../img/project2.webp";
+import project3 from "../img/project3.webp";
+import project4 from "../img/project4.webp";
+import project5 from "../img/project5.webp";
+import project6 from "../img/project6.webp";
+import project7 from "../img/project7.webp";
 
 const projects = [
   { src: project1, alt: "Проєкт 1" },
-  // ...
+  { src: project2, alt: "Проєкт 2" },
+  { src: project3, alt: "Проєкт 3" },
+  { src: project4, alt: "Проєкт 4" },
+  { src: project5, alt: "Проєкт 5" },
+  { src: project6, alt: "Проєкт 6" },
+  { src: project7, alt: "Проєкт 7" },
 ];
 
 const PortfolioPage = () => {
