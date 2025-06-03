8d0d72df3c310ef4be69317929284bd1b8f915d0
gdvolya <gdvolya@gmail.com>
2025-06-03 09:56:59 +0300

    Refactor: ленивые маршруты и Firebase только для AdminPanel, обновления Lighthouse отчетов

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,19 +1,19 @@
-module.exports = { 
+module.exports = {
   ci: {
     collect: {
       url: ['https://promelektroservice-react-structure.vercel.app/'],
-      numberOfRuns: 3
+      numberOfRuns: 3,  // Количество запусков для получения стабильных данных
     },
     upload: {
-      target: 'filesystem',
-      outputDir: '.lighthouseci/report'
+      target: 'filesystem', // Сохраняем отчёты на файловой системе
+      outputDir: '.lighthouseci/report', // Указываем каталог для хранения отчётов
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.3 }],
-        'categories:accessibility': ['warn', { minScore: 0.9 }],
-        'categories:seo': ['warn', { minScore: 0.9 }]
-      }
-    }
-  }
+        'categories:performance': ['error', { minScore: 0.3 }], // Минимальная оценка для производительности
+        'categories:accessibility': ['warn', { minScore: 0.9 }], // Минимальная оценка для доступности
+        'categories:seo': ['warn', { minScore: 0.9 }], // Минимальная оценка для SEO
+      },
+    },
+  },
 };
