58da1eeb49cbf1cbe70fd8d7dd4f6eb589511382
gdvolya <gdvolya@gmail.com>
2025-07-09 17:42:53 +0300

    🚀 Релиз

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,19 +1,26 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['https://promelektroservice-react-structure.vercel.app/'],
-      numberOfRuns: 3,  // Количество запусков для получения стабильных данных
+      staticDistDir: "./build",
+      url: ["https://promelektroservice-react-structure.vercel.app/"],
+      numberOfRuns: 3,
+      settings: {
+        preset: "desktop"
+      }
     },
     upload: {
-      target: 'filesystem', // Сохраняем отчёты на файловой системе
-      outputDir: '.lighthouseci/report', // Указываем каталог для хранения отчётов
+      target: "filesystem",
+      outputDir: ".lighthouseci/report",
+      reportFilenamePattern: "promelektroservice_%-date%.report.html"
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.3 }], // Минимальная оценка для производительности
-        'categories:accessibility': ['warn', { minScore: 0.9 }], // Минимальная оценка для доступности
-        'categories:seo': ['warn', { minScore: 0.9 }], // Минимальная оценка для SEO
-      },
-    },
-  },
+        "categories:performance": ["error", { minScore: 0.6 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        "categories:seo": ["warn", { minScore: 0.9 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
+        "categories:pwa": "off" // Отключено, если сайт не PWA
+      }
+    }
+  }
 };
