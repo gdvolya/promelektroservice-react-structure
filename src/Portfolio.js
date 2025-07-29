ed0f8b144242edf806770af8ecd15892e973e7e4
gdvolya <gdvolya@gmail.com>
2025-07-29 13:13:34 +0300

    🚀 Релиз

--- src/Portfolio.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Portfolio() {
-  return <div><h1>Портфолио</h1></div>;
-}
--- src/Pricing.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Pricing() {
-  return <div><h1>Цены</h1></div>;
-}
